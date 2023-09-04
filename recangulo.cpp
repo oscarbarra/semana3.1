c3eca84b1e47fa80e1b4c0c2785ae9dfeba48eae
oscarbarra <obarra2023@alu.uct.cl>
2023-09-04 11:44:10 -0300

    c2_rectangulo

--- recangulo.cpp
+++ recangulo.cpp
@@ -7,6 +7,14 @@ class Rectangulo{
     public:
     int width, heigth;
 
+    void valorWidht(){
+        cout << "ingresar el valor del ancho: "; cin >> width;
+    }
+
+    void valorHeight(){
+        cout << "ingresar el valor del alto: "; cin >> heigth;
+    }
+
     void area(){
         cout << width * heigth << endl;
     }
@@ -14,14 +22,27 @@ class Rectangulo{
     void perimetro(){
         cout << 2 * (width + heigth) << endl;
     }
+
+    void cambiarValorWidht(){
+        cout << "ingresar el nuevo valor del ancho: "; cin >> width;
+    }
+
+    void cambiarValorHeght(){
+        cout << "ingresar el nuevo valor del alto:  "; cin >> heigth;
+    }
 };
 
 
 int main(){
     //nombre de la instancia
     Rectangulo myObj;
-    myObj.width = 10;
-    myObj.heigth = 5;
+    myObj.valorWidht();
+    myObj.valorHeight();
     myObj.area();
     myObj.perimetro();
+    myObj.cambiarValorWidht();
+    myObj.cambiarValorHeght();
+    myObj.area();
+    myObj.perimetro();
+
 }
