d85b3d51a080bbe62cce88010f4e906b75975095
oscarbarra <obarra2023@alu.uct.cl>
2023-09-04 11:37:32 -0300

    c2_circulo

--- circulo.cpp
+++ circulo.cpp
@@ -7,21 +7,32 @@ class Circulo{
     public:
     int pi, radio;
 
+    void valorRadio(){
+        cout << "ingresar el valor del radio: "; cin >> radio;
+    }
+
     //pow permite elever a 'n' numero
     void area(){
         cout << pi * (pow(radio, 2)) << endl;
     }
 
     void perimetro(){
-        cout << pi * radio * 2;
+        cout << pi * radio * 2 << endl;
+    }
+
+    void cambiarRadio(){
+        cout << "ingresar el nueva valor del radio: "; cin >> radio;
     }
 };
 
 
 int main(){
     Circulo myObj;
-    myObj.radio = 3;
     myObj.pi = 3.1415;
+    myObj.valorRadio();
+    myObj.area();
+    myObj.perimetro();
+    myObj.cambiarRadio();
     myObj.area();
     myObj.perimetro();
 }
